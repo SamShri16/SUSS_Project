0d2d90b6e4dff21714b5d9762fcd1d8d611eedcc
SamShri16 <samarthshrivastava05@gmail.com>
2026-04-16 20:28:46 +0530

    Progress update: templates, base layout, backend setup in progress

--- suss_project/notes/models.py
+++ suss_project/notes/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Note(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     content = models.TextField()
+    pinned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
