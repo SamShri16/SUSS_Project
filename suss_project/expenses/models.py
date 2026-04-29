8f075b42584222cbd0f22382d0333dcde67c5ff1
SamShri16 <samarthshrivastava05@gmail.com>
2026-04-29 23:57:39 +0530

    Added Income model and reset database

--- suss_project/expenses/models.py
+++ suss_project/expenses/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 class Expense(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.FloatField()
     category = models.CharField(max_length=100)
-    date = models.DateField()
+    date = models.DateField(default=now)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount}"
+
 
 class Income(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.FloatField()
     source = models.CharField(max_length=100)
-    date = models.DateField()
+    date = models.DateField(default=now)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount}"
